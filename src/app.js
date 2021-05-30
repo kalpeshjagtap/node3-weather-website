17ac8c859d0c6132dda86ba7a41cdfa3eb03e35c
Kalpesh <kalpeshjagtap52@gmail.com>
2021-05-30 12:02:24 +0530

    change the help text

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('Help', {
-        helpText: 'How May I Help You?',
+        helpText: 'How May I Help You',
         title: 'Help',
         name: 'Kalpesh Jagtap'
     })
